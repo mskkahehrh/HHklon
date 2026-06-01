8b2bb56f0caa6237ee5624ab6d0eb704ca8312ea
mskkahehrh <ishanxbot@gmail.com>
2026-06-01 15:48:48 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -37,7 +37,7 @@ const config = {
   PREFIX: '.',
   MAX_RETRIES: 3,
   GROUP_INVITE_LINK: 'https://chat.whatsapp.com/HRIlSELlxO5JQo2gYL4MzV?mode=gi_t',
-  RCD_IMAGE_PATH: 'https://files.catbox.moe/qb2puf.jpeg',
+  RCD_IMAGE_PATH: 'https://raw.githubusercontent.com/ishanxmd/Natu/refs/heads/main/file_000000002e7c71faa522e326c9553ea2.png',
   NEWSLETTER_JID: 'jid',
   OTP_EXPIRY: 300000,
   WORK_TYPE: 'public',
